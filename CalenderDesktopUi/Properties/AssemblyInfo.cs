349abb271c19e838786f0c936385b848137da701
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-24 00:24:28 +0330

    - Update AssmblyInfo Project

--- CalenderDesktopUi/Properties/AssemblyInfo.cs
+++ CalenderDesktopUi/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Windows;
 [assembly: AssemblyTitle("CalenderDesktopUi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AminMirzaeiOne")]
 [assembly: AssemblyProduct("CalenderDesktopUi")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("AminMirzaeiOne")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
